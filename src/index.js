223bce901fbcb367ff5ec59d8d4cd44331f14c4a
deboraviveiros <deviveiros.debora@gmail.com>
2023-12-08 14:18:17 -0300

    Códigos HTML e CSS + início JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import analyzer from './analyzer.js';
 
+document.getElementById("reset-button").addEventListener("click", limpaTexto)
+
+function limpaTexto() {
+    if (document.getElementsByName("user-input")).value != ""
+}
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
