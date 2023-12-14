8788b4578f9a61cc2365a27432d3bf58e1a95a54
Monica <monicasayama@gmail.com>
2023-12-14 13:22:23 -0300

    Revert "🐞 fix: change input to textarea"
    This reverts commit cc2618c4d64c971ed43869a657b7e26e9c8d7439.

--- read-only/test/oa/oa-web-api.spec.js
+++ read-only/test/oa/oa-web-api.spec.js
@@ -84,9 +84,9 @@ describe('Uso de selectores del DOM', () => {
 });
 
 describe('Manejo de eventos del DOM', () => {
-  it('Se registra un Event Listener para el evento "textarea"', () => {
+  it('Se registra un Event Listener para el evento "input"', () => {
     expect(
-      addEventListenerCalls.some((node) => node.arguments[0].value === "textarea")
+      addEventListenerCalls.some((node) => node.arguments[0].value === "input")
     ).toBeTruthy();
   });
 
