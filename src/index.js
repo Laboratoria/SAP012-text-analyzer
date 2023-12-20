0690e739349c2257a0f5db9a60ea0b980e7f1e5d
deboraviveiros <deviveiros.debora@gmail.com>
2023-12-20 18:53:18 -0300

    20/12/23

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+import { clean } from 'gh-pages';
 import analyzer from './analyzer.js';
 
-function listaContadores() {
-    const metricas = document.getElementsByName("user-input");
-    metricas.querySelector(".user-input").addEventListener("count")
-}
-// para limpar o texto, preciso:
-
-
+const caixa = document.querySelector("textarea");
+const conteudo = document.querySelector("p");
 
 document.getElementById("reset-button").addEventListener("click", limparTexto);
 
 function limparTexto() {
-    console.log("executou função")
-
-
+    console.log("executou função");
+    if{
+        document.querySelector("p").value != "",
+        //executar limpeza de texto e mostrar caixa de texto atualizada
+        caixa.textContent = "Digite seu texto aqui";
+    } else {
+        // não fazer nada ou atualizar página
+    }
 }
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// para limpar o texto, preciso que:
+//1) o botão funcione; ok
+//2) o código descubra se a textarea está preenchida ou não;
+//3) desconsidere o comando se a textarea estiver vazia;
+//4) substitua o texto pela mensagem "Digite seu texto aqui"
+
+// para contar caracteres, preciso que:
+// 1) o código leia a textarea e descubra se seu conteúdo é diferente de 0;
+// 2)
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto 'analyzer'
