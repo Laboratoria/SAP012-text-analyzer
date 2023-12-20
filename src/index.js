2a249452922425e0983a467e53da1462d77bf110
deboraviveiros <deviveiros.debora@gmail.com>
2023-12-20 08:58:45 -0300

    Eventos DOM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import analyzer from './analyzer.js';
 
 function listaContadores() {
     const metricas = document.getElementsByName("user-input");
-    metricas.querySelector(".u")
+    metricas.querySelector(".user-input").addEventListener("count")
 }
 // para limpar o texto, preciso:
 
@@ -12,6 +12,7 @@ document.getElementById("reset-button").addEventListener("click", limparTexto);
 
 function limparTexto() {
     console.log("executou função")
+
 
 }
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
