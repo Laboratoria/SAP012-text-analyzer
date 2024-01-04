752d0a2474ac5e5964e1d8892f19777ffb57b6bb
deboraviveiros <deviveiros.debora@gmail.com>
2024-01-04 16:00:42 -0300

    analyzer [numbers]

--- src/analyzer.js
+++ src/analyzer.js
@@ -5,7 +5,6 @@
 };
 */
 
-
 const analyzer = {
   getWordCount: (text) => {
     const cortaPalavra = text.split(" ");
@@ -21,20 +20,28 @@ const analyzer = {
   },
   getAverageWordLength: (text) => {
     /*  for (let i = 0; i < cortaPalavra.length; i++) {
-        compMedio += cortaPalavra[i].length;
+        compMedio += cortaPalavra[i].length.parseFloat(text);
       }*/
   },
   getNumberCount: (text) => {
-    const number = parseInt(text);
-
-    if (text === Number) {
-      return // total de números presentes no texto
-    } else {
-      return;
+    const number = text.replace(/[^0-9]/g, '');
+    for (let i = 0; i >= number./*o quê?? */; i++) {
+      return number[i++];
     }
+
+    // if (text === Number) {
+    //   return parseInt(number);
+    // } else {
+    //   return "0";
+    // 
+    // //;
+
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    const somaNum = parseInt(text) + parseFloat(text);
+    // reduce(), mas não sei onde
+
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
