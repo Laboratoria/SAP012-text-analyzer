d5da181401081d5acf74924b7fd8d462baf51306
deboraviveiros <deviveiros.debora@gmail.com>
2024-01-05 12:01:03 -0300

    contadores de números e caracteres sem espaços ok

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,47 +1,42 @@
-
-/*for (let i = 0; areaTexto.length >= i; i++) {
-  console.log("testou o loop");
-  document.querySelectorAll(".counter").innerHTML = i++ + "";
-};
-*/
-
 const analyzer = {
   getWordCount: (text) => {
     const cortaPalavra = text.split(" ");
     return cortaPalavra.length;
   },
+  //o que eu queria fazer: o contador não pode contar os espaços como palavras e não pode iniciar em 1;
   getCharacterCount: (text) => {
     return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
-
+    const espacos = text.replaceAll(" ", "").length;
+    return espacos;
 
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
   getAverageWordLength: (text) => {
-    /*  for (let i = 0; i < cortaPalavra.length; i++) {
-        compMedio += cortaPalavra[i].length.parseFloat(text);
-      }*/
+    //   const apenasPalavras = text.split(" ");
+    //   if (apenasPalavras.length > 0) {
+    //     return parseFloat(apenasPalavras);
+    //   } else {
+    //     return "0";
+    //   }
   },
+
+  // compMedio += cortaPalavra[i].length.parseFloat(text);
+  // return compMedio.valueOf(text);
   getNumberCount: (text) => {
-    const number = text.replace(/[^0-9]/g, '');
-    for (let i = 0; i >= number./*o quê?? */; i++) {
-      return number[i++];
+    const numbers = text.replace(/[^0-9]/g, '');
+    if (parseInt(numbers) >= 0) {
+      return numbers.length;
+    } else {
+      return "0";
     }
-
-    // if (text === Number) {
-    //   return parseInt(number);
-    // } else {
-    //   return "0";
-    // 
-    // //;
-
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
+  //},
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   getNumberSum: (text) => {
     const somaNum = parseInt(text) + parseFloat(text);
     // reduce(), mas não sei onde
-
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
